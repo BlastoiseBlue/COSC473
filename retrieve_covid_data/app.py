5ad0a0a3c99e3f219dd5f4bed9a8b6616ef41cd5
Emmet Stanevich <emmet@stanevich.com>
2020-12-07 23:31:13 -0500

    Implemented filtering into data retrieval
    The lambda function will now, instead of retrieving data on a state level, retrieve data on a county level. Additionally, it also filters the summaries to only include the counties our client is interested in.

--- retrieve_covid_data/app.py
+++ retrieve_covid_data/app.py
@@ -1,8 +1,8 @@
 import boto3
-from io import BytesIO
-import requests
 import os
 import json
+import pandas as pd
+from urllib.error import HTTPError
 
 
 def lambda_handler(event, context):
@@ -24,26 +24,26 @@ def lambda_handler(event, context):
     ------
         The same input event file
     """
-    s3 = boto3.resource("s3")
-    bucket = s3.Bucket("bigdata2bucket")
     sec = boto3.client("secretsmanager", region_name="us-east-1")
     secret = json.loads(
         sec.get_secret_value(SecretId=os.environ["SECRET_NAME"]).get("SecretString")
     )
-    with requests.get(
-        "https://api.covidactnow.org/v2/states.csv",
-        params={"apiKey": secret["API_KEY"]},
-    ) as r, BytesIO(r.content) as payload:
-        try:
-            r.raise_for_status()
-            if r.ok:
-                bucket.upload_fileobj(payload, "covid-retrieval-test.csv")
-            return {
-                "statusCode": 200,
-                "body": f"Update {'successful' if r.ok else 'failed'}, received status code {r.status_code}!",
-            }
-        except requests.exceptions.HTTPError as err:
-            return {
-                "statusCode": 500,
-                "body": f"Update unsuccessful, received response {err}",
-            }
+    try:
+        target_counties = pd.read_parquet(
+            "s3://bigdata2bucket/county-list.parquet", columns=["API FIPS"]
+        )["API FIPS"]
+        dtypes = {
+            "country": "category",
+            "state": "category",
+            "level": "category",
+            "metrics.testPositivityRatioDetails.source": "category",
+        }
+        pd.read_csv(
+            f"https://api.covidactnow.org/v2/counties.csv?apiKey={secret['API_KEY']}",
+            dtype=dtypes,
+            index_col=["fips"],
+        ).sort_index().loc[target_counties].reset_index().to_csv("s3://bigdata2bucket/counties.csv")
+    except HTTPError as err:
+        return {"statusCode": 500, "body": f"Update unsuccessful, received response {err}"}
+    else:
+        return {"statusCode": 200, "body": "Update successful!"}
